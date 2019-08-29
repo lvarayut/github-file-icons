9099bb8c69a463bf10a0c588f81734bcfb9157ae
Grammatopoulos Athanasios-Vasileios <gramthanos@gmail.com>
2019-08-29 22:50:08 +0300

    Added `replace-in-file-webpack-plugin` to auto-fix file-icons-js's style.css
    Also the hard coded extension's id was changed to the dynamic `__MSG_@@extension_id__` so that the extension also works as an unpacked extension (with other id)

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ReplaceInFileWebpackPlugin = require('replace-in-file-webpack-plugin');
 
 module.exports = {
 	entry: {
@@ -13,20 +14,34 @@ module.exports = {
 	},
 	plugins: [
 		new CopyWebpackPlugin([{
+			from: 'node_modules/file-icons-js/css/style.css',
+			to: 'vendors/file-icons-js/css/style.css'
+		}]),
+
+		new CopyWebpackPlugin([{
 			from: '*',
 			context: 'src',
 			ignore: '*.js'
 		}, {
 			from: 'icons/',
 			to: 'icons/'
-    }, {
-      from: 'vendors/**',
-    }, {
-      from: 'node_modules/file-icons-js/fonts/',
-      to: 'vendors/file-icons-js/fonts/'
-    }, {
+		}, {
+			from: 'vendors/**',
+		}, {
+			from: 'node_modules/file-icons-js/fonts/',
+			to: 'vendors/file-icons-js/fonts/'
+		}, {
 			from: 'node_modules/jquery/dist/jquery.slim.min.js',
-      to: 'vendors/jquery.slim.min.js'
+			to: 'vendors/jquery.slim.min.js'
+		}]),
+
+		new ReplaceInFileWebpackPlugin([{
+			dir: 'dist/vendors/file-icons-js/css/',
+			files: ['style.css'],
+			rules: [{
+				search: /url\("\.\.\/fonts\//g,
+				replace: 'url("chrome-extension://__MSG_@@extension_id__/vendors/file-icons-js/fonts/'
+			}]
 		}])
 	]
 };

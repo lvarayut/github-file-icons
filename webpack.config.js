5e98c8f6abee82dcb3d58e1fdb32d649c75a21b4
Varayut Lerdkanlayanawat <l.varayut@gmail.com>
2020-05-20 21:04:37 +0700

    Upgrade copy-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -3,31 +3,40 @@ const webpack = require('webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-	entry: {
-		content: './src/content',
-		bg: './src/bg',
-	},
-	output: {
-		path: path.join(__dirname, 'dist'),
-		filename: '[name].js'
-	},
-	plugins: [
-		new CopyWebpackPlugin([{
-			from: '*',
-			context: 'src',
-			ignore: '*.js'
-		}, {
-			from: 'icons/',
-			to: 'icons/'
-    }, {
-      from: 'vendors/**',
-    }, {
-      from: 'node_modules/file-icons-js/fonts/',
-      to: 'vendors/file-icons-js/fonts/'
-    }, {
-			from: 'node_modules/jquery/dist/jquery.slim.min.js',
-      to: 'vendors/jquery.slim.min.js'
-		}])
-	]
+  entry: {
+    content: './src/content',
+    bg: './src/bg'
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: '*',
+          context: 'src',
+          globOptions: {
+            ignore: '*.js'
+          }
+        },
+        {
+          from: 'icons/',
+          to: 'icons/'
+        },
+        {
+          from: 'vendors/**'
+        },
+        {
+          from: 'node_modules/file-icons-js/fonts/',
+          to: 'vendors/file-icons-js/fonts/'
+        },
+        {
+          from: 'node_modules/jquery/dist/jquery.slim.min.js',
+          to: 'vendors/jquery.slim.min.js'
+        }
+      ]
+    })
+  ]
 };
-
